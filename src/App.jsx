431e24e730d264aa17a3c7043ac463af866f11eb
Hugo Romerito <hugoxvida@gmail.com>
2022-06-03 02:49:46 -0300

    Foi adicionado a sidebar, a seção de posts e a biblioteca phosphor. A seção de posts ainda está sem estilização, apenas a sidebar foi estilizada

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import './global.css'
 import { Header } from "./components/header/Header";
+import { Post } from './Post';
 
-function App() {
+import styles from './App.module.css'
 
+import './global.css'
+import { Sidebar } from "./components/Sidebar/Sidebar";
+
+export function App() {
   return (
-    <Header />
-  )
-}
+    <>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Hugo Romerito"
+            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto suscipit eius ratione veniam modi beatae velit porro fuga veritatis exercitationem adipisci perferendis possimus assumenda culpa quasi, dolorem molestiae hic quo!"
+          />
 
-export default App
+          <Post
+            author="Romerito Queiroz"
+            content="um novo post muito legal!"
+          />
+        </main>
+      </div>
+    </>
+  );
+}
